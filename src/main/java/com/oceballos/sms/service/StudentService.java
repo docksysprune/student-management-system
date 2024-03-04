8e4a78e8fef841aa2acc7976985e095aecce1b0a
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2024-03-04 16:38:32 -0600

    Logics for updating a student

--- src/main/java/com/oceballos/sms/service/StudentService.java
+++ src/main/java/com/oceballos/sms/service/StudentService.java
@@ -7,4 +7,7 @@ import java.util.List;
 public interface StudentService {
     List<StudentDto> getAllStudents();
     void createStudent(StudentDto studentDto);
+    StudentDto getStudentById(Long studentId);
+
+    void updateStudent(StudentDto studentDto);
 }
