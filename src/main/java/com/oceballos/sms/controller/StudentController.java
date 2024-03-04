cbcea8be9fd79c9144eecee3e0995b054094fdd4
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2024-03-04 16:03:43 -0600

    Changes on UI for the students main table.

--- src/main/java/com/oceballos/sms/controller/StudentController.java
+++ src/main/java/com/oceballos/sms/controller/StudentController.java
@@ -2,8 +2,10 @@ package com.oceballos.sms.controller;
 
 import com.oceballos.sms.dto.StudentDto;
 import com.oceballos.sms.service.StudentService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -34,7 +36,11 @@ public class StudentController {
     }
 
     @PostMapping("/students")
-    public String saveStudent(@ModelAttribute("student") StudentDto student){
+    public String saveStudent(@Valid @ModelAttribute("student") StudentDto student, BindingResult result, Model model){
+        if(result.hasErrors()) {
+            model.addAttribute("student",student);
+            return "create_student";
+        }
         studentService.createStudent(student);
         return "redirect:/students";
     }
